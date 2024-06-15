6b63011056412fa2b43c3903df26050d565f5215
Talal-Sharaa <talal.i.sharaa@gmail.com>
2024-06-16 01:32:24 +0300

    Added color_to_black task

--- color_to_black/color_to_black.py
+++ color_to_black/color_to_black.py
@@ -1,25 +1,15 @@
 from PIL import Image
 import sys
 def color_to_black(img_path):
-    # Load the image
     img = Image.open(img_path)
-    img = img.convert("RGB")  # Ensure image is in RGB format
-
-    # Get image dimensions
+    img = img.convert("RGB")
     width, height = img.size
-
-    # Create a new image for the grayscale result
     grayscale_img = Image.new("L", (width, height))
 
     for x in range(width):
         for y in range(height):
-            # Get the RGB values of the pixel
             r, g, b = img.getpixel((x, y))
-
-            # Calculate the grayscale value
             gray = int(0.2989 * r + 0.5870 * g + 0.1140 * b)
-
-            # Set the grayscale value in the new image
             grayscale_img.putpixel((x, y), gray)
 
     return grayscale_img
